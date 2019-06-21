bed17726a8550e0c67eec087f231bac8012e9a4e
ihakapink <ihakapink@gmail.com>
2019-06-21 21:23:28 +1200

    added route for cards

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -12,3 +12,13 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
   db.addCard(req.body)
 })
+
+router.get('/', (req, res) => {
+  db.getCards()
+    .then(cards => {
+      res.send(cards)
+    })
+    .catch(err => {
+      res.status(500).send('DATABASE ERROR: ' + err.message)
+    })
+})
