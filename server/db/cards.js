cc728595cd9c84fd10474623893a6c06decf1f6d
ihakapink <ihakapink@gmail.com>
2019-06-21 21:21:05 +1200

    added update card

--- server/db/cards.js
+++ server/db/cards.js
@@ -3,19 +3,32 @@ const config = require('../db/knexfile')[enviornment]
 const connection = require('knex')(config)
 
 module.exports = {
-    getCard,
-    addCard
+  getCard,
+  addCard,
+  getCards,
+  updateCard
 
 }
 
 function getCard (id, db = connection) {
-    return db('cards')
-    .where('userId', id )
+  return db('cards')
+    .where('userId', id)
     .first()
 }
 
 function addCard (newCard, db = connection) {
-    return db('cards')
+  return db('cards')
     .insert(newCard)
 }
 
+function getCards (db = connection) {
+  return db('cards')
+    .join('categories', 'categories.id', 'cards.categoryId')
+    .select('cards.id', 'cards.categoryId', 'cards.id', 'cards.answer', 'cards.dateCreated')
+}
+
+function updateCard (updatedCard, db = connection) {
+  return db('cards')
+    .where('id', updateCard.id)
+    .update(updatedCard)
+}
